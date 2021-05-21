5596e2ea9d8f691c658e2ef9ff6a78d74f732366
Leon <45981520+doobietime@users.noreply.github.com>
2021-05-21 16:23:42 +1200

    Update skuController.php

--- app/Http/Controllers/skuController.php
+++ app/Http/Controllers/skuController.php
@@ -50,50 +50,120 @@ class skuController extends Controller
         $getUser = \Auth::user()->name;
         $skuCode = $request->get('sc');
         $type = $request->get('pt');
-        $find = \App\Sku::where('Code', $skuCode)->where('product_type', $type)->count();
-   
-        if($type === "TBC" && $find == 0)
+        $find = \App\Sku::where('Code', $skuCode)->get();
+
+        if($type == "RM")
         {
-            \App\Sku::create([
+            $findRM = \App\Sku::where('Code', $skuCode)->where('product_type','RM')->first();
+            if($findRM)
+            {
+                return redirect()->back()->with('error','RM SKU already exists');
+            }
+            else
+            {
+                \App\Sku::create([
+                'Code' => $request->get('sc'),
+                'Description' => $request->get('desc'),
+                'target_range_ind_min' => $request->get('imin'),
+                'target_range_ind_max' => $request->get('imax'),
+                'target_range_row_min' => $request->get('rmin'),
+                'target_range_row_max' => $request->get('rmax'),
+                'product_type' => $request->get('pt'),
+                'supplier' => $request->get('sup'),
+                ]);
+                return redirect()->back()->with('message','sku has been added !');
+            }
+
+        }
+        if($type !== "RM")
+        {
+            $findNRM = \App\Sku::where('Code', $skuCode)->where('product_type','!=','RM')->first();
+            if($findNRM)
+            {
+                return redirect()->back()->with('error','SKU already exists');
+            }
+            else
+            {
+                \App\Sku::create([
+                'Code' => $request->get('sc'),
+                'Description' => $request->get('desc'),
+                'target_range_ind_min' => $request->get('imin'),
+                'target_range_ind_max' => $request->get('imax'),
+                'target_range_row_min' => $request->get('rmin'),
+                'target_range_row_max' => $request->get('rmax'),
+                'product_type' => $request->get('pt'),
+                'supplier' => $request->get('sup'),
+                ]);
+
+                \App\Sku::create([
                 'Code' => $request->get('sc'),
                 'Description' => $request->get('desc'),
                 'product_type' => 'TBC',
                 'product_sub_type' => 'pouch',
                 ]);
                 return redirect()->back()->with('message','sku has been added !');
+            }
         }
-        if($type === "RM" && $find == 0)
-        {
-            \App\Sku::create([
-                'Code' => $request->get('sc'),
-                'Description' => $request->get('desc'),
-                'target_range_ind_min' => $request->get('imin'),
-                'target_range_ind_max' => $request->get('imax'),
-                'target_range_row_min' => $request->get('rmin'),
-                'target_range_row_max' => $request->get('rmax'),
-                'product_type' => $request->get('pt'),
-                'supplier' => $request->get('sup'),
-                ]);
+        
+        // $arr = [];
+        // foreach($find as $f)
+        // {
+        //    array_push($arr,$f->product_type);
+        // }
+        // $result = array_search("asdf",$arr);
 
-        }
-        if($type !=="RM" && $type !=="TBC" && $find == 0)
-        {
-            \App\Sku::create([
-                'Code' => $request->get('sc'),
-                'Description' => $request->get('desc'),
-                'target_range_ind_min' => $request->get('imin'),
-                'target_range_ind_max' => $request->get('imax'),
-                'target_range_row_min' => $request->get('rmin'),
-                'target_range_row_max' => $request->get('rmax'),
-                'product_type' => $request->get('pt'),
-                'supplier' => $request->get('sup'),
-                ]);
+        // if($type == "RM")
+        // {
+        //     if($find !== null){
+        //     return redirect()->back()->with('error','SKU already exists');
+        //     }
+        //     else
+        //     {
+        //         \App\Sku::create([
+        //             'Code' => $request->get('sc'),
+        //             'Description' => $request->get('desc'),
+        //             'target_range_ind_min' => $request->get('imin'),
+        //             'target_range_ind_max' => $request->get('imax'),
+        //             'target_range_row_min' => $request->get('rmin'),
+        //             'target_range_row_max' => $request->get('rmax'),
+        //             'product_type' => $request->get('pt'),
+        //             'supplier' => $request->get('sup'),
+        //             ]);
+        //             return redirect()->back()->with('message','sku has been added !');
+        //     }
 
-        }
-        else
-        {
-            return redirect()->back()->with('error','SKU already exists');
-        }
+        // }
+        // //if not RM
+        // if($type !== "RM")
+        // {
+        //     if($find !== null && $type !== "RM"){
+        //         return redirect()->back()->with('error','SKU already exists');
+        //     }
+        //     else
+        //     {
+        //         //create process check sku
+        //         \App\Sku::create([
+        //         'Code' => $request->get('sc'),
+        //         'Description' => $request->get('desc'),
+        //         'product_type' => 'TBC',
+        //         'product_sub_type' => 'pouch',
+        //         ]);
+        //         //create QC check sku
+        //         \App\Sku::create([
+        //         'Code' => $request->get('sc'),
+        //         'Description' => $request->get('desc'),
+        //         'target_range_ind_min' => $request->get('imin'),
+        //         'target_range_ind_max' => $request->get('imax'),
+        //         'target_range_row_min' => $request->get('rmin'),
+        //         'target_range_row_max' => $request->get('rmax'),
+        //         'product_type' => $request->get('pt'),
+        //         'supplier' => $request->get('sup'),
+        //         ]);
+        //         return redirect()->back()->with('message','sku has been added for both process and quality checks');
+        //     }
+
+        // }
+        //ARG
 
         // if($type != "RM" && $find !== null){
         //     return redirect()->back()->with('error','SKU already exists');
